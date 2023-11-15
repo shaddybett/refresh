1cd978966f2e7ae19ae6a7d552c88a850309e229
Shaddy <shadybett540@gmail.com>
2023-11-15 08:07:08 +0300

    added another component which is ontact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Body from './Body'
 import { Link } from 'react-router-dom'
 export default function Contact() {
   return (
     <div>
-       
+       <Link to='/body'>back</Link>
         <h3>shadrack.bett.92@gmail.com</h3>
     </div>
   )
